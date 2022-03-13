667ea2911d6b145aa0288ae4b7723a3b4ff1e70a
imarov11 <saidibakaskambarbekovich@gmail.com>
2022-03-13 19:07:59 +0300

    first commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	   int sum = 1 ;
+        for (int w = 1 ; w <= 5; w++){
+             sum *= w;
+
+            System.out.print (sum + " ");
+    }
+
+    }
+}
